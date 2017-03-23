8bf869d474cf8a72018d8580361ae038a8805666
Nat Kale <nathanielkale@gmail.com>
2017-03-23 11:36:20 -0700

    Plot reorganized to show multiple water years overlain on top of one another.

--- js/plot.js
+++ js/plot.js
@@ -5,6 +5,9 @@
 // D3 line chart constants
 //  See https://bl.ocks.org/mbostock/3883245
 
+// Multiple x-scales used to plot all water years on the same axis
+//   See http://stackoverflow.com/questions/42870187/d3-multi-series-chart-time-year-interval-x-axis-overlaying-multiple-years-of/42870609
+
 var parseDate = d3.timeParse("%Y-%m-%d %H:%M:%S");
 
 var dailyData = {};
@@ -17,12 +20,25 @@ var svg = d3.select("svg")
     height = +svg.attr("height") - margin.top - margin.bottom,
     g = svg.append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-var x = d3.scaleTime();
-
+//var x = d3.scaleTime();
+var x_scales = {};
 var y = d3.scaleLinear();
 
+var thisYear;
+
+function color(years, year) {
+    c = "";
+    if (+year == +_.max(years)) {
+        c = "#525252";
+    } else {
+        c = "#cccccc";
+    };
+    
+    return c;
+};
+
 var line = d3.line()
-    .x(function(d) { return x(d.day); })
+    .x(function(d) { return x_scales[thisYear](d.day); })
     .y(function(d) { return y(d.val); });
 
 // Helper function that takes a date object and calculates the water year
@@ -55,24 +71,43 @@ function plotSite(data, g_id) {
       // Wipe out the old graph
       d3.select("g.x-axis").remove();
       d3.select("g.y-axis").remove();
-      d3.select("path").remove();
-      
+      svg.selectAll('path.valueLine').remove();
       
       // Only show data for the site we've selected; also sort by day.
       data = _.filter(data, {"G_ID" : g_id});
-      data = _.sortBy(data, ["day"]);
+      //data = _.sortBy(data, ["day"]);
+      data_wy = _.groupBy(data, 'wy');
+      
+      years = _.keys(data_wy);
+      data_plot = [];
+      
+      // Create separate x scales for each water year.  They need to have the
+      //   same range, but the domain will be different; that allows different
+      //   dates to map to the same x coordinate, which is what we want.
+      years.forEach(function(d, i) {
+          data_plot.push({
+              year: d,
+              points: _.sortBy(data_wy[d], ["day"])
+          })
+      });
+      
+      years.forEach(function(d) {
+          x_scales["scale" + d] = d3.scaleTime()
+            .domain([new Date(d-1, 10, 1), new Date(d, 9, 30)])
+            .rangeRound([margin.left, width]);
+      });
       
       // Set up the range; important that it be inside the function for resizing
-      x.rangeRound([0, width]);
+      //x.rangeRound([0, width]);
       y.rangeRound([height, 0]);
       
-      x.domain(d3.extent(data, function(d) { return d.day; }));
+      //x.domain(d3.extent(data, function(d) { return d.day; }));
       y.domain(d3.extent(data, function(d) { return d.val; }));
 
       // Add the x-axis to the plot.  Us a class to identify it later.
       g.append("g")
           .attr("transform", "translate(0," + height + ")")
-          .call(d3.axisBottom(x))
+          .call(d3.axisBottom(x_scales['scale' + _.max(years)]))
           .attr("class", "x-axis")
         .select(".domain")
           .remove();
@@ -88,16 +123,22 @@ function plotSite(data, g_id) {
           .attr("dy", "0.71em")
           .attr("text-anchor", "end")
           .text("Value");
-
-      // Add the actual data (line) to the graph.
-      g.append("path")
-          .datum(data)
-          .attr("fill", "none")
-          .attr("stroke", "steelblue")
+      
+      // Add multiple lines to the graph; one for each water year
+      g.selectAll('valueLine')
+        .data(data_plot)
+        .enter()
+        .append('path')
+          .attr('class', 'valueLine')
+          .attr('stroke', function(d,i) { return color(years, d.year)})
+          .attr("d", function(d) {
+              thisYear = 'scale' + d.year;
+              return line(d.points)})
           .attr("stroke-linejoin", "round")
           .attr("stroke-linecap", "round")
           .attr("stroke-width", 1.5)
-          .attr("d", line);
+          .attr("fill", "none")
+      
     };
 };
 

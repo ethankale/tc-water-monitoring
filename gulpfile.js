663ab2e2ac9922abb2c7cccab84e90010eb33279
Nat Kale <nathanielkale@gmail.com>
2017-03-28 09:30:09 -0700

    New Gulp tasks for building and deploying.

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,21 @@
 var gulp = require('gulp');
 var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var sourcemaps = require('gulp-sourcemaps');
 
-gulp.task('scripts', function() {
-  return gulp.src(['./js/selectsite.js', './js/map.js', './js/plot.js'])
+gulp.task('build', function() {
+    return gulp.src(['./js/selectsite.js', './js/map.js', './js/plot.js'])
+    .pipe(sourcemaps.init())
     .pipe(concat('all.js'))
+    .pipe(uglify())
+    .pipe(sourcemaps.write('.'))
     .pipe(gulp.dest('./js/'));
 });
 
-gulp.task('default', function() {
-  runSequence('scripts');
+
+gulp.task('deploy', function() {
+    return gulp.src(["./*"])
+    .pipe(gulp.dest('\\\\rsh2o\\Web_Water-Monitoring\\'));
 });
 
 

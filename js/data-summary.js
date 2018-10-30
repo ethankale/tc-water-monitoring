99afa6e0d7850a13d22f997f973823d690811399
Nat Kale <nathanielkale@gmail.com>
2018-10-30 09:37:42 -0700

    some layout changes to tiles.

--- js/data-summary.js
+++ js/data-summary.js
@@ -3,6 +3,17 @@
 
 function createDischargeSummary(site, data, type="Stage") {
     
+    // Clear the previous data
+    var el_list = document.querySelectorAll("#summary_container p")
+    
+    for (var i=0; i<el_list.length; i++) {
+        var el = el_list[i];
+        el.innerHTML = "";
+    }
+    
+    document.getElementById('sparkline').innerHTML = "";
+    
+    // Calculated values to fill in tile display
     if (type == "Stage") {
         param = "val";
         digits = 2;
@@ -29,11 +40,12 @@ function createDischargeSummary(site, data, type="Stage") {
     
     
     // Make a sparkline style graph.
+    var daysPast = 7;
+    
     var lastIndex = _.findIndex(data, {'day': lastRecord.day})
-    var sliced_data = data.slice(lastIndex-5, lastIndex)
+    var sliced_data = data.slice(lastIndex-daysPast, lastIndex)
     var spark_data = _.map(sliced_data, function(d) { return {'x': d.dt, 'y':d[param]} } );
     
-    document.getElementById('sparkline').innerHTML = "";
     var sparkchart = new Chartist.Line('#sparkline', 
       {
         series: [{
@@ -58,11 +70,13 @@ function createDischargeSummary(site, data, type="Stage") {
     document.querySelector("#tile1 .summary-info").innerHTML = Number(lastVal).toFixed(digits) ;
     document.querySelector("#tile1 .summary-details").innerHTML = "Latest on " + maxDate.format("Y-MM-DD");
     
+    document.querySelector("#tile2 .summary-details").innerHTML = "Last " + daysPast + " days of measurement";
+    
     document.querySelector("#tile3 .summary-info").innerHTML = yearsMeasured;
     document.querySelector("#tile3 .summary-details").innerHTML = "Years Monitored";
     
     document.querySelector("#tile4 .summary-info").innerHTML = Number(minVal).toFixed(digits) + "  -  " + Number(maxVal).toFixed(digits);
-    document.querySelector("#tile4 .summary-details").innerHTML = "Minimum to Maximum";
+    document.querySelector("#tile4 .summary-details").innerHTML = "Minimum and Maximum";
 
     
     
